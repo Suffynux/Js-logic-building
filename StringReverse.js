7e66df99a78742312f41dbf8ce3f5d29da127a09
suffynux <suffynux@gmail.com>
2025-06-19 13:37:45 +0500

    string method added

--- StringReverse.js
+++ StringReverse.js
@@ -10,8 +10,30 @@ const reverseString = (value) => {
     }
     return result;
   } else {
-    return "Value should be string onlly";
+    throw new Error("Value should be string onlly")
   }
 };
 
 console.log(reverseString(232));
+
+// String reverse using array method
+const reverseString2 = (value) => {
+  // Check for the value should be string
+  if (typeof value == "number") {
+    console.log("Value should be a string");
+    return;
+  }
+  // for spliting the array means breaking the string and saving into an array
+  const splitedArray = value.split("");
+  // It will reverse the array
+  const reversedString = splitedArray.reverse();
+  // For joining the splited value
+
+  const finalResult = reversedString.join("");
+
+  console.log("Reverse value is ", finalResult);
+  return;
+};
+
+// reverseString2("name");
+reverseString2(12);
